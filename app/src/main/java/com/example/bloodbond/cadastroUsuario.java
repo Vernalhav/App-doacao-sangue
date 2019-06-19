9f4b5ffeeb5fdce8040f78683316b56268feb09d
Laura Genari <44553125+LauraGenari@users.noreply.github.com>
2019-06-19 20:49:53 -0300

    Add files via upload

--- app/src/main/java/com/example/bloodbond/cadastroUsuario.java
+++ app/src/main/java/com/example/bloodbond/cadastroUsuario.java
@@ -5,7 +5,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class cadastroUsuario extends Fragment {
@@ -13,10 +17,15 @@ public class cadastroUsuario extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.activity_cadastro_usuario, container, false);
+        final View view = inflater.inflate(R.layout.activity_cadastro_usuario, container, false);
 
-        TextView tv = view.findViewById(R.id.editText);
-        //tv.setText("CADASTRO USUARIO");
+        final EditText nome = view.findViewById(R.id.name);
+        final EditText senha = view.findViewById(R.id.senha);
+        final EditText idade = view.findViewById(R.id.idade);
+        final EditText peso = view.findViewById(R.id.peso);
+        final EditText email = view.findViewById(R.id.email);
+        final EditText altura = view.findViewById(R.id.altura);
+        final Spinner spinner = view.findViewById(R.id.spinner);
 
         Button btnCadastroUsuario = (Button)view.findViewById(R.id.button3);
         btnCadastroUsuario.setOnClickListener(new View.OnClickListener() {
@@ -24,7 +33,7 @@ public class cadastroUsuario extends Fragment {
             public void onClick(View v) {
 
                 // TODO: Criar uma instancia da classe pessoa com os dados do novo usuario.
-                 Pessoa pessoa = new Pessoa("test2@hotmal.com", "2444666666", "test2", 2, 65, 232, 22);
+                 Pessoa pessoa = new Pessoa(email.getText(), senha.getText(), nome.getText(), spinner.getSelectedItem().toString(), peso.getText(), altura.getText(), idade.getText());
 
                 // TODO: Descomentar função.
                 DatabaseManager.getInstance().RegisterUser(pessoa);
@@ -32,8 +41,6 @@ public class cadastroUsuario extends Fragment {
             }
         });
 
-        
-        Spinner spinner = view.findViewById(R.id.spinner);
 
         ArrayAdapter<CharSequence> adapter = (ArrayAdapter<CharSequence>) ArrayAdapter.createFromResource(this.getContext(), R.array.blood_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -42,13 +49,14 @@ public class cadastroUsuario extends Fragment {
         return view;
     }
 
-    @Override
+    //@Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String text = parent.getItemAtPosition(position).toString();
     }
 
-    @Override
+    //@Override
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
 }
