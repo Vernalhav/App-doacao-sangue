96674f44913de807f5de41ec54ba2e565718e5bd
Victor <40337396+Vernalhav@users.noreply.github.com>
2019-06-19 15:20:47 -0300

    Adicionei data e doador, funcao que diz se a doacao ja foi

--- app/src/main/java/com/example/bloodbond/Doacao.java
+++ app/src/main/java/com/example/bloodbond/Doacao.java
@@ -1,19 +1,41 @@
 package com.example.bloodbond;
 
 import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Doacao implements Serializable {
 
     private double qtDoacao;    // em mL
     private Instituicao instDestino;
+    private Pessoa doador;
     private int tipoSanguineo;  // "macros" definidos em com.example.firebasetest.Instituicao.java
+    private Date data;
 
     public Doacao(){}
 
-    public Doacao(double qtDoacao, int tipoSanguineo, Instituicao inst) {
+    public Doacao(double qtDoacao, int tipoSanguineo, Pessoa doador, Instituicao inst, Date data) {
         this.qtDoacao = qtDoacao;
+        this.doador = doador;
         this.instDestino = inst;
         this.tipoSanguineo = tipoSanguineo;
+        this.data = data;
+    }
+
+    public Pessoa getDoador() {
+        return doador;
+    }
+
+    public void setDoador(Pessoa doador) {
+        this.doador = doador;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
     }
 
     public double getQtDoacao() {
@@ -38,5 +60,15 @@ public class Doacao implements Serializable {
 
     public void setTipoSanguineo(int tipoSanguineo) {
         this.tipoSanguineo = tipoSanguineo;
+    }
+
+    /**
+     * Funcao que determina se
+     * a doacao já foi realizada
+     * no tempo presente
+     * @return se a dooacao foi realizada
+     */
+    public boolean jaFoi(){
+        return data.compareTo(Calendar.getInstance().getTime()) <= 0;
     }
 }
